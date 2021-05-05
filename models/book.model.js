b7009a0aeea0bdb703e06fd2d83242839325fcb2
Shubham Singh <>
2021-05-05 16:34:21 +0000

    fix: inventory page

--- models/book.model.js
+++ models/book.model.js
@@ -15,6 +15,7 @@ const BookSchema = new Schema({
   inStock: Boolean
 });
 
-const Book = mongoose.model('book', BookSchema);
+// const Book = mongoose.model('book', BookSchema);
+const Book = mongoose.model('product', BookSchema);
 
 module.exports = Book;
